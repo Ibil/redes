a66aa314d0820f107d8613ea36b54d0f9829133c
wkdesktop <wrongkiller@gmail.com>
2015-09-29 19:46:58 +0100

    Tes recebe RQS falta testar

--- TESf/TES.c
+++ TESf/TES.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <time.h>
 
 #define DEFAULT_TES_PORT 59000
 #define DEFAULT_ECP_PORT 58018
@@ -36,6 +37,7 @@ static int QID_index = 0;
 typedef struct{
 	int SID;
 	int QID;
+	time_t t_registo;
 	int Score;
 }quest_form;
 
@@ -119,23 +121,6 @@ void tcp_connect(){
 	return;
 }
 
-/*void tcp_read(char* buffer, int nbytestoread){
-	printf("Vou ler a mensagem\n");
-	ptr=buffer;
-	do{
-		if((nread = read(newfd,ptr,1)) == -1){
-			printf("Erro a ler a mensagem\n");
-			exit(1);
-		}
-		printf("O char : %c\n", (*ptr));
-		ptr+=nread;
-	}
-	while( (*(ptr-1))!='\n' );
-	
-	printf("recebi a mensagem : %s\n", buffer);
-	return;
-}*/
-
 void tcp_read(char *buffer, int nbytestoread){
 	char *ptr;
 	nleft=nbytestoread;
@@ -151,6 +136,28 @@ void tcp_read(char *buffer, int nbytestoread){
 		ptr+=nread;
 	}
 	printf("A resposta lida foi : %s\n", buffer);
+	return;
+}
+
+void tcp_read_alt(char *msg){
+	char *ptr_temp;
+	
+	printf("Vou ler a mensagem\n");
+	ptr_temp=&msg[0];
+	
+	
+	do{
+		if((nread = read(newfd,ptr_temp,1)) == -1){
+			printf("Erro a ler a mensagem\n");
+			exit(1);
+		}
+		printf("O char : %c\n", (*ptr_temp));
+		ptr_temp+=nread;
+	}
+	while( (*(ptr_temp-1))!=' ' );
+	
+	puts("sair do read alt");
+	printf("A resposta lida foi: %s\n", msg);
 	return;
 }
 
@@ -186,7 +193,34 @@ void limpa_buffer(char* buffer, int tamanho){
 	return;
 }
 
+char* verifica_respostas(char* vec_respostas){
+	FILE *fp_txt;
+	
+	fp_txt = fopen("1QF1A", "r");
+	fread(vec_respostas,sizeof(char),9,fp_txt);
+	
+	fclose(fp_txt);
+	
+	return vec_respostas;
+}
 
+int verifica_score(char* input){
+	int i = 0;
+	char *respostas;
+	int score = 0;
+	
+	respostas = (char*)malloc(5*sizeof(char));
+	
+	verifica_respostas(respostas);
+	
+	while( i< 9){
+		if( input[i] == respostas[i] ){
+			score+= 20;
+		}
+		i+= 2;
+	}
+	return score;
+}
 
 int conta_digitos_int(int numero){
 	int n_digitos;
@@ -229,45 +263,49 @@ char* get_dados(char* dados, int file_size){
 void tcp_envia_AQT(int QID){
 	long int file_size;
 	char s_QID[25];
+	struct tm tm_registo;
+	char aqt_time[18];
 	char *dados;
 	int n_digitos;
 	char *s_size_of_data;
 	
-	tcp_write("AQT ", 4);
+	static const char mon_name[][4] = {
+    	"JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+    	"JUL", "AUG", "SEP", "OCT", "NOV", "DEC"
+ 	};
 	
 	limpa_buffer(s_QID, 25);
+	
+	tcp_write("AQT ", 4);
+	
 	sprintf(s_QID,"%d", QID);
-	printf("################### %d\n\n", strlen(s_QID));
-
 	tcp_write(s_QID, strlen(s_QID));
 	tcp_write(" ", 1);
 	
+	/*guarda tempo actual*/
+	time( &(db_quest_form[QID].t_registo) );  
+	/* copia conteudo para outra estrutura de modo a imprimir */
+  	tm_registo = *localtime( &(db_quest_form[QID].t_registo) );
+  	
+  	sprintf(aqt_time, "%.2d%.3s%.4d_%.2d:%.2d:%.2d",
+    	tm_registo.tm_mday, mon_name[tm_registo.tm_mon], 1900 + tm_registo.tm_year,
+   		tm_registo.tm_hour, tm_registo.tm_min, tm_registo.tm_sec);
 	
-	/* fazer set_time
-	time: DDMMMYYYY_HH:MM:SS
-		  09JAN2016_20:00:00
-	*/
-	printf("que raio e set_time???\n");
-	tcp_write("09JAN2016_20:00:00", 18);
+	tcp_write(aqt_time, 18);
 	tcp_write(" ", 1);
-	
 	
 	file_size = get_file_size();
 	n_digitos = conta_digitos_long_int(file_size);
-
 	s_size_of_data = (char*)malloc(n_digitos * sizeof(char));
 	sprintf(s_size_of_data, "%ld", file_size);
-	
 	tcp_write( s_size_of_data, n_digitos);
 	tcp_write(" ", 1);
-	
 	
 	dados=(char*)malloc(file_size*sizeof(char));
 	limpa_buffer(dados,file_size);
 	dados = get_dados(dados, file_size);
 	tcp_write(dados,file_size);
 	free(dados);
-	
 	
 	tcp_write("\n", 1);
 	return;	
@@ -287,25 +325,69 @@ void tcp_trata_mensagem(){
 	
 	int studID;
 	char s_stud_ID[5];
+	int QID;
+	char s_QID[24];
+	char respostas[10];
+	int score;
 	
 	tcp_input_buffer = (char*)malloc(sizeof(char)* 10+ QID_SIZE + 12);
 	limpa_buffer(tcp_input_buffer, 27);
+	limpa_buffer(s_QID, 24);
+	limpa_buffer(respostas, 10);
 	
 	tcp_read(tcp_input_buffer, 4);
 	printf("buffer: %s", tcp_input_buffer);
 	if( !strcmp("RQT ", tcp_input_buffer)){
 		/* Le o SID e grava */
-		printf("entrei no RQT\n");
+		printf("Li o RQT' '\n");
 		tcp_read(s_stud_ID, 5);
 		studID =atoi(s_stud_ID);
 		/* come o '\n'*/
 		tcp_read(tcp_input_buffer, 1);
 		
-		
-		tcp_envia_AQT(cria_instancia_QID(studID));
+		QID = cria_instancia_QID(studID);
+		tcp_envia_AQT(QID);
 	}
 	else{
-		printf("Fazer o RQS!!\n");
+		printf("Pedido em TCP-RQT do user mal formatado\n");
+		free(tcp_input_buffer);
+		return;
+	}
+	
+	/*Recebe RQS */
+	limpa_buffer(tcp_input_buffer, 27);
+	tcp_read(tcp_input_buffer, 4);
+	printf("buffer: %s", tcp_input_buffer);
+	if( !strcmp("RQS ", tcp_input_buffer)){
+		printf("LI o RQS' '\n");
+		
+		/* Le o SID*/
+		tcp_read(s_stud_ID, 5);
+		studID =atoi(s_stud_ID);
+		/* le o espaco*/
+		tcp_read(tcp_input_buffer, 1);
+		
+		/* LE o QID*/
+		tcp_read_alt(s_QID);
+		s_QID[strlen(s_QID)-1] = '\0'; 	/* substitui ' ' do read_alt por '\0' */
+		QID = atoi(s_QID);
+		
+		/*calcula_pontuacao();*/
+		tcp_read(respostas,10);
+		score = verifica_score(respostas); 
+		
+		printf("$$$$$$$$$$$$$$$$$$$$$ O score foi : %d\n\n", score);
+		/* falta verificar o time*/
+		
+		/*responde ao User*/
+		printf("Fazer o resto!!\n\n");
+		
+		/* informa o ECP*/
+		printf("Fazer o resto!!\n\n");
+		
+		
+	}else{
+		printf("Pedido em TCP-RQS do user mal formatado\n");
 	}
 	
 	free(tcp_input_buffer);
